59e10458ae8268724151216f47e94b23b5ebf8ca
ajcudd <ajcuddeback@gmail.com>
2020-12-17 10:37:29 -0500

    added special cursor for desktop and laptop devices

--- intro.js
+++ intro.js
@@ -67,3 +67,12 @@ window.addEventListener('resize', function () {
     canvas.height = innerHeight;
     init();
 })
+
+const mouse = document.querySelector(".cursor")
+
+function cursor(e) {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px"
+}
+
+window.addEventListener('mousemove', cursor)
